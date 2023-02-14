c4ee5303c43e931eac4198b81d5a179eb55342ab
Laxman kendre <106816249+lkendre5652@users.noreply.github.com>
2023-02-14 16:59:30 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './common/Home';
+import { Contact } from './common/Contact';
+import {NavLink,Routes,Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(<>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/contact">Contact</NavLink>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />     
+    </Routes>
+ </>)
 }
-
 export default App;
